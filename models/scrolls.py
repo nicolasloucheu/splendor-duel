db11529e7691372eb15989369ae508be6ced4402
nicolasloucheu <nicolas.loucheu@hotmail.com>
2023-11-02 12:13:04 +0100

    better algorithm to place scrolls on board

--- models/scrolls.py
+++ models/scrolls.py
@@ -1,4 +1,5 @@
 from kivy.graphics import Ellipse
+from kivy.metrics import dp
 from kivy.uix.widget import Widget
 
 
@@ -7,19 +8,19 @@ class Scrolls(Widget):
         super(Scrolls, self).__init__(**kwargs)
         self.scrolls = 3
         self.orientation = 'vertical'
-        self.circle1 = Ellipse(size=(50, 50))
-        self.circle2 = Ellipse(size=(50, 50))
-        self.circle3 = Ellipse(size=(50, 50))
+        self.circe_size = dp(20)
+        self.circle1 = Ellipse(size=(self.circe_size, self.circe_size))
+        self.circle2 = Ellipse(size=(self.circe_size, self.circe_size))
+        self.circle3 = Ellipse(size=(self.circe_size, self.circe_size))
+        self.circles = [self.circle1, self.circle2, self.circle3]
         self.canvas.add(self.circle1)
         self.canvas.add(self.circle2)
         self.canvas.add(self.circle3)
 
     def on_size(self, *args):
-        print('on size: ' + str(self.width) + ', ' + str(self.height))
-        print(self.center_x, self.center_y)
-        self.circle1.pos = (self.center_x - 25, self.height*1/4 - 25)
-        self.circle2.pos = (self.center_x - 25, self.height*2/4 - 25)
-        self.circle3.pos = (self.center_x - 25, self.height*3/4 - 25)
+        circle_positions = [(self.center_x - 25, self.height * i / 10 - 25) for i in range(4, 4 + self.scrolls)]
+        for circle, position in zip(self.circles, circle_positions):
+            circle.pos = position
 
     def draw_scroll(self):
         self.scrolls -= 1
