91e3ab90525af553a81875303788fd5ab70493a9
nicolasloucheu <nicolas.loucheu@hotmail.com>
2023-11-09 15:46:27 +0100

    aded owned_tokens

---
+++ models/owned_tokens.py
@@ -0,0 +1,27 @@
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+
+from models.token import GemType
+
+
+class OwnedTokens(BoxLayout):
+    tokens = {GemType.RED: 0, GemType.GREEN: 0, GemType.BLUE: 0, GemType.BLACK: 0, GemType.WHITE: 0, GemType.PEARL: 0,
+              GemType.GOLD: 0}
+
+    def __init__(self, **kwargs):
+        super(OwnedTokens, self).__init__(**kwargs)
+        red_tokens = Label(text=str(self.tokens[GemType.RED]) + ' red')
+        self.add_widget(red_tokens)
+        green_tokens = Label(text=str(self.tokens[GemType.GREEN]) + ' green')
+        self.add_widget(green_tokens)
+        blue_tokens = Label(text=str(self.tokens[GemType.BLUE]) + ' blue')
+        self.add_widget(blue_tokens)
+        black_tokens = Label(text=str(self.tokens[GemType.BLACK]) + ' black')
+        self.add_widget(black_tokens)
+        white_tokens = Label(text=str(self.tokens[GemType.WHITE]) + ' white')
+        self.add_widget(white_tokens)
+        pearl_tokens = Label(text=str(self.tokens[GemType.PEARL]) + ' pearl')
+        self.add_widget(pearl_tokens)
+        gold_tokens = Label(text=str(self.tokens[GemType.GOLD]) + ' gold')
+        self.add_widget(gold_tokens)
+
--- models/player.py
+++ models/player.py
@@ -1,15 +1,18 @@
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 
+from models.owned_tokens import OwnedTokens
+
 
 class Player(GridLayout):
+
     def __init__(self, **kwargs):
         super(Player, self).__init__(**kwargs)
         self.cols = 2
         self.size_hint = (.75, .5)
         self.pos_hint = {'center_x': .5}
         size_top = (1, .33)
-        owned_tokens = Button(text='owned_tokens', size_hint=size_top)
+        owned_tokens = OwnedTokens(size_hint=size_top)
         self.add_widget(owned_tokens)
         owned_scrolls = Button(text='owned_scrolls', size_hint=size_top)
         self.add_widget(owned_scrolls)
