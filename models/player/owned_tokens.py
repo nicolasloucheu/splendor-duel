56649e395b4d8d214d2c9a2d2b706bb75d1086a9
nicolasloucheu <nicolas.loucheu@hotmail.com>
2023-11-28 09:35:11 +0100

    add owned tokens in a for loop

--- models/player/owned_tokens.py
+++ models/player/owned_tokens.py
@@ -5,11 +5,11 @@ from models.unit.token import GemType
 
 
 class OwnedTokens(BoxLayout):
-
     def __init__(self, **kwargs):
         super(OwnedTokens, self).__init__(**kwargs)
         self.tokens = {GemType.RED: 0, GemType.GREEN: 0, GemType.BLUE: 0, GemType.BLACK: 0, GemType.WHITE: 0,
                        GemType.PEARL: 0, GemType.GOLD: 0}
+        self.token_widgets = {}
         self.update_widgets()
 
     def add_tokens(self, tokens_list):
@@ -30,17 +30,9 @@ class OwnedTokens(BoxLayout):
 
     def update_widgets(self):
         self.clear_widgets()
-        red_tokens = Label(text=str(self.tokens[GemType.RED]) + ' red')
-        self.add_widget(red_tokens)
-        green_tokens = Label(text=str(self.tokens[GemType.GREEN]) + ' green')
-        self.add_widget(green_tokens)
-        blue_tokens = Label(text=str(self.tokens[GemType.BLUE]) + ' blue')
-        self.add_widget(blue_tokens)
-        black_tokens = Label(text=str(self.tokens[GemType.BLACK]) + ' black')
-        self.add_widget(black_tokens)
-        white_tokens = Label(text=str(self.tokens[GemType.WHITE]) + ' white')
-        self.add_widget(white_tokens)
-        pearl_tokens = Label(text=str(self.tokens[GemType.PEARL]) + ' pearl')
-        self.add_widget(pearl_tokens)
-        gold_tokens = Label(text=str(self.tokens[GemType.GOLD]) + ' gold')
-        self.add_widget(gold_tokens)
+        self.token_widgets = {}
+        for color in GemType:
+            if color != GemType.ANY:
+                token_color = Label(text=str(self.tokens[color]) + ' ' + str(color))
+                self.token_widgets[color] = token_color
+                self.add_widget(token_color)
